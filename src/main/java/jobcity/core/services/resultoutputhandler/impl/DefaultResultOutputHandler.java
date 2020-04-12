577562c06cbe8269eac27a0d48dab0935281f8e2
tomasmendesr <tomasmendesr@gmail.com>
2020-04-12 19:40:02 -0300

    fix prints

--- src/main/java/jobcity/core/services/resultoutputhandler/impl/DefaultResultOutputHandler.java
+++ src/main/java/jobcity/core/services/resultoutputhandler/impl/DefaultResultOutputHandler.java
@@ -32,7 +32,7 @@ public class DefaultResultOutputHandler implements ResultOutputHandler {
         final Map<Player, List<Frame>> framesByPlayer = playerService.getFramesByPlayer();
         framesByPlayer.forEach((player, frames) -> {
             System.out.printf("%10s %10s %10s %10s %10s %10s %10s %10s %10s %10s %10s\n", player.getName(), "", "", "", "", "", "", "", "", "", "");
-            System.out.printf("%10s %10s %10s %10s %10s %10s %10s %10s %10s %10s %10s\n", "Pinfalls", getFramePinfallsOutput(frames.get(0)), getFramePinfallsOutput(frames.get(1)), getFramePinfallsOutput(frames.get(2)), getFramePinfallsOutput(frames.get(3)), getFramePinfallsOutput(frames.get(4)), getFramePinfallsOutput(frames.get(5)), getFramePinfallsOutput(frames.get(6)), getFramePinfallsOutput(frames.get(7)), getFramePinfallsOutput(frames.get(8)), getFramePinfallsOutput(frames.get(9)));
+            System.out.printf("%10s %10s %10s %10s %10s %10s %10s %10s %10s %10s %10s\n", "Pinfalls", getFramePinfallsOutput(frames.get(0)), getFramePinfallsOutput(frames.get(1)), getFramePinfallsOutput(frames.get(2)), getFramePinfallsOutput(frames.get(3)), getFramePinfallsOutput(frames.get(4)), getFramePinfallsOutput(frames.get(5)), getFramePinfallsOutput(frames.get(6)), getFramePinfallsOutput(frames.get(7)), getFramePinfallsOutput(frames.get(8)), getLastFrameOutput(frames));
             System.out.printf("%10s %10s %10s %10s %10s %10s %10s %10s %10s %10s %10s\n", "Score", "", "", "", "", "", "", "", "", "", "", "", "");
         });
     }
@@ -53,5 +53,26 @@ public class DefaultResultOutputHandler implements ResultOutputHandler {
         if (pinfalls.size() == 1) return "    X";
         if (firstShoot + secondShoot == 10) return " " + firstShoot + "  /";
         return " " + firstShoot + "  " + secondShoot;
+    }
+
+    private String getLastFrameOutput(final List<Frame> frames) {
+        Frame frame9 = frames.get(9);
+        final List<Pinfall> pinfallsFromFrame9 = pinfallService.findByFrame(frame9);
+        if (pinfallsFromFrame9.size() == 1) {
+            String result = " X";
+            Frame frame10 = frames.get(10);
+            final List<Pinfall> pinfallsFromFrame10 = pinfallService.findByFrame(frame10);
+            if (pinfallsFromFrame10.get(0).getQuantity() == 10) {
+                result = result + " X ";
+                Frame frame11 = frames.get(11);
+                final List<Pinfall> pinfallsFromFrame11 = pinfallService.findByFrame(frame10);
+                if (pinfallsFromFrame11.get(0).getQuantity() == 10) {
+                    return result + "X";
+                }
+                return result + pinfallsFromFrame11.get(0).getQuantity();
+            }
+            return result + " " + pinfallsFromFrame10.get(0).getQuantity() + " " + pinfallsFromFrame10.get(1).getQuantity();
+        }
+        return getFramePinfallsOutput(frame9);
     }
 }
